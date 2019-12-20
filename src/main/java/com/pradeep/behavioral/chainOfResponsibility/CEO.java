c5476a94027154bd7fb0ae57fdc2de89c2e13f77
pradeepkumaresan <pradeep.it39@gmail.com>
2019-12-20 19:02:34 +0530

    chain of responsibility

---
+++ src/main/java/com/pradeep/behavioral/chainOfResponsibility/CEO.java
@@ -0,0 +1,9 @@
+package com.pradeep.behavioral.chainOfResponsibility;
+
+public class CEO extends Handler {
+
+	@Override
+	public void handleRequest(Request request) {
+		System.out.println("CEOs can approve anything they want");
+	}
+}
---
+++ src/main/java/com/pradeep/behavioral/chainOfResponsibility/RequestType.java
@@ -0,0 +1,5 @@
+package com.pradeep.behavioral.chainOfResponsibility;
+
+public enum RequestType {
+	CONFERENCE, PURCHASE;
+}
